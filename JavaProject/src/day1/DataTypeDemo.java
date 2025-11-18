18007f2b9abae196144c1b0a487de3a78c3d93b1
Aravind Juloori <julooriaravind@gmail.com>
2025-11-18 23:07:51 +0530

    committing the variables and data types demo

---
+++ JavaProject/src/day1/DataTypeDemo.java
@@ -0,0 +1,65 @@
+package day1;
+
+public class DataTypeDemo {
+
+	public static void main(String[] args) {
+		
+		//Data Types- represents which type of data we are storing in a variable
+		//Types of data 1.Primitive data type 2.Non-primitive data type
+		
+		//Numeric data types
+		
+		int a=100 ,b=200;
+		
+		System.out.println("The value of a is : " +a);
+		System.out.println("The value of b is : " +b);
+		System.out.println("The Sum of a and b is :" +(a+b));
+		
+		
+		
+		byte by=125;   //-128 to 127
+		System.out.println(by);
+		
+		short sh=3535;  //-32768 to -32767
+		System.out.println(sh);
+		
+		long l=123123123232432L;
+		System.out.println(l);
+		
+		
+		float f=123.232f;
+		System.out.println(f);
+		
+		
+		double dbl=1234.23232323;
+		System.out.println(dbl);
+		
+		char grade='A';
+		System.out.println(grade);
+		
+		String name="John";
+		System.out.println(name);
+		
+		// char ch='ABC';  //error
+		//String ch='ABC'; //error
+	//	String ch="A"; //Valid
+	//	String ch='A' //in valid
+		
+		
+		boolean b1=false;   //allows only true/false
+		System.out.println(b1);
+		
+		
+		//boolean b2="true"; //invalid
+		//String v1=true;//invalid
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
--- JavaProject/src/day1/FirstJavaProgram.java
+++ JavaProject/src/day1/FirstJavaProgram.java
@@ -5,6 +5,8 @@ public class FirstJavaProgram {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World");
+		System.out.println(10+20);  //30 is the output
+		System.out.println("10+20");  //10+20 is the output
 
 	}
 
