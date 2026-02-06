e6fc3d608685e3ea5305c73924db5809661b55ba
Aravind Juloori <Julooriaravind@gmail.com>
2026-02-06 07:45:58 +0530

    Pushing the This and static keyword concepts

---
+++ JavaProject/src/day13/StaticMain.java
@@ -0,0 +1,10 @@
+package day13;
+
+public class StaticMain {
+
+	public static void main(String[] args) {
+
+		System.out.println(StaticKeyword.a);
+	}
+
+}
---
+++ JavaProject/src/day13/ThisKeyword.java
@@ -0,0 +1,37 @@
+package day13;
+
+public class ThisKeyword {
+	
+	int x,y;
+	
+	
+	/*
+	 * ThisKeyword(int x,int y){ this.x=x; this.y=y;
+	 * 
+	 * }
+	 */
+	
+	void setData(int x,int y) {
+		this.x=x;
+		this.y=y;
+	}
+	
+	void display() {
+		System.out.println(x);
+		System.out.println(y);
+	}
+
+	public static void main(String[] args) {
+		
+		//ThisKeyword tk=new ThisKeyword(100,200);
+		ThisKeyword th=new ThisKeyword();
+		th.setData(100, 100);
+		th.display();
+		
+		
+		
+		
+
+	}
+
+}
